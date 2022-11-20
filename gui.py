cba40a8bbc2971ae412b8f16a232308e47323c72
Shivam <shivammehrotra09@yahoo.in>
2022-11-20 21:54:58 +0530

    Removed Image for ADD and COMPLETE button to cater UI

--- gui.py
+++ gui.py
@@ -1,17 +1,22 @@
 import function
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", 'w') as file:
+        pass
 
 sg.theme("DarkGreen3")
 
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in your To-Do")
 input_box = sg.InputText(tooltip="Enter To-Do", key="todo")
-add_button = sg.Button(size=3, image_source="add.png", mouseover_colors="LightBlue2", tooltip="Add To-Do", key="Add")
+add_button = sg.Button("Add")
 list_box = sg.Listbox(values=function.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
-complete_button = sg.Button(size=10, image_source="complete.png", key="Complete")
+complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
