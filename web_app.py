5493b616e8de6bccba42643a51db7bd17a15984d
Shivam <shivammehrotra09@yahoo.in>
2022-11-20 22:26:54 +0530

    Implement add new to-do on web

--- web_app.py
+++ web_app.py
@@ -3,10 +3,18 @@ import function
 
 todos = function.get_todos()
 
+
+def add_todo():
+    todo_new = st.session_state["new_todo"] + "\n"
+    todos.append(todo_new)
+    function.write_todos(todos)
+
+
 st.title("My To-Do App")
 st.subheader("Place all your daily tasks here!!!")
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a To-Do", placeholder="add to-do")
+st.text_input(label="Enter a To-Do", placeholder="add to-do",
+              on_change=add_todo, key='new_todo')
